929ddb49abd7ee22878b387329833cbe8fb68ec8
KrishPatel23 <69263323+KrishPatel23@users.noreply.github.com>
2020-09-07 19:47:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+constructer(body1,body2,offsetX,offsetY)
+{
+this.offsetX=offsetX
+this.offsetY=offsetY
+
+var options={
+
+bodyA:body1,
+bodyB:body2,
+pointB:{x:this.offsetX, y:this.offsetY}
+}
+this.rope=Constraint.create(options)
+World.add(world,this.rope)
+}
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2,0)
+  
+  drawSprites();
+ 
+}
+
+
+
